41cda231bfeef1a60d425ac74d651893a829705d
Hany Fekry <hanyaly_82@hotmail.com>
2024-08-05 15:58:30 +0300

    Create handler and endpoint for GetProductsQuery

---
+++ src/Services/CatalogApi/Products/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace CatalogApi.Products.GetProducts
+{
+    //public record GetProductsRequest();
+    public record GetProductsResponse(IReadOnlyList<ProductDto> ProductDtos);
+    public class GetProductsEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet($"/products", async (ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductsQuery());
+                return Results.Ok(result.Adapt<GetProductsResponse>());
+            })
+                .WithName("GetProducts")
+                .Produces<GetProductsResponse>(statusCode: StatusCodes.Status200OK)
+                .ProducesProblem(statusCode: StatusCodes.Status400BadRequest)
+                .WithDescription("Get all products")
+                .WithSummary("Get products");
+        }
+    }
+}
---
+++ src/Services/CatalogApi/Products/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace CatalogApi.Products.GetProducts
+{
+    public record GetProductsQuery() : IQuery<GetProductsResult>;
+    public record ProductDto(Guid Id, string Name, string Description, List<string> Category, double Price);
+    public record GetProductsResult(IReadOnlyList<ProductDto> ProductDtos);
+    public class GetProductsHandler(IDocumentSession session, ILogger<GetProductsHandler> logger) : IQueryHandler<GetProductsQuery, GetProductsResult>
+    {
+        public async Task<GetProductsResult> Handle(GetProductsQuery request, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductsHandler.Handle called with {@request}", request);
+            var result = await session.Query<Product>().ToListAsync();
+            return new GetProductsResult(result.Adapt<IReadOnlyList<ProductDto>>());
+        }
+    }
+}
