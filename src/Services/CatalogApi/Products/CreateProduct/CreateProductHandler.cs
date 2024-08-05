123afc1931dc37e76687f963a3872cd3264c2644
Hany Fekry <hanyaly_82@hotmail.com>
2024-08-05 13:28:59 +0300

    add Create productHandler using Matren package to save product object to postgresql db

---
+++ src/Services/CatalogApi/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,24 @@
+﻿using BuildingBlocks.CQRS;
+using CatalogApi.Models;
+using Marten;
+
+namespace CatalogApi.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, string Description, List<string> Category, double Price,
+        string ImageUrl) : ICommand<CreateProductResult>;
+
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            //map CreateProductCommand to Product
+            //Save Product to database.
+            var entity = request.Adapt<Product>();
+            session.Store(entity);
+            await session.SaveChangesAsync();
+
+            return new CreateProductResult(entity.Id);
+        }
+    }
+}
