25b66ab2b81ebd008faf144208bd896cfec9320b
Hany Fekry <hanyaly_82@hotmail.com>
2024-08-05 13:19:10 +0300

    Add building block project as base CQRS Requests and handlers

---
+++ src/BuldingBlocks/CQRS/IQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace BuildingBlocks.CQRS
+{
+    public interface IQueryHandler<in TQuery> : IQueryHandler<TQuery, Unit> where TQuery : IQuery
+    {
+
+    }
+    public interface IQueryHandler<in TQuery, TResponse> : IRequestHandler<TQuery, TResponse>
+        where TQuery : IQuery<TResponse>
+        where TResponse : notnull
+    {
+    }
+}
