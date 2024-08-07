ca1c8785e77fd5054b1f4815a2456f03120b8001
Hany Fekry <hanyaly_82@hotmail.com>
2024-08-07 16:45:26 +0300

    Register CustomExceptionHandler in program.cs

--- src/Services/CatalogApi/Program.cs
+++ src/Services/CatalogApi/Program.cs
@@ -1,25 +1,31 @@
 using BuildingBlocks.Behaviours;
+using CatalogApi.Exceptions.Handlers;
 using FluentValidation;
+using Microsoft.AspNetCore.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 var assembly = typeof(Program).Assembly;
+//register MediatR for CQRS
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(assembly);
     config.AddOpenBehavior(typeof(ValidationBehaviour<,>));
 });
-
+//register fluent validation
 builder.Services.AddValidatorsFromAssembly(assembly);
-
+//add Carter for minimal API
 builder.Services.AddCarter();
+//add Marten to use Postgre sql as document DB
 builder.Services.AddMarten(config =>
 {
     config.Connection(builder.Configuration.GetConnectionString("DefaultConnection")!);
 })
     .UseLightweightSessions();
+//add custom exception handler
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 var app = builder.Build();
 
