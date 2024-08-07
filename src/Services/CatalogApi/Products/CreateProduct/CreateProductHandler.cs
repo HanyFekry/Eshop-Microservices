6c83a5dbb224ad94944a76536c4bc8b9e2d660fa
Hany Fekry <hanyaly_82@hotmail.com>
2024-08-07 14:36:20 +0300

    add CreateProductCommandValidator

--- src/Services/CatalogApi/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/CatalogApi/Products/CreateProduct/CreateProductHandler.cs
@@ -1,11 +1,23 @@
 ﻿
 
+using FluentValidation;
+
 namespace CatalogApi.Products.CreateProduct
 {
     public record CreateProductCommand(string Name, string Description, List<string> Category, double Price,
         string ImageUrl) : ICommand<CreateProductResult>;
 
     public record CreateProductResult(Guid Id);
+
+    public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommandValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("{PropertyName} is required");
+            RuleFor(x => x.Category).NotEmpty().WithMessage("{PropertyName} is required");
+            RuleFor(x => x.Price).GreaterThan(0).WithMessage("{PropertyName} is required");
+        }
+    }
     internal class CreateProductHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
