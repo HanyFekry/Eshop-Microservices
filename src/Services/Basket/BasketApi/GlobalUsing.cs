952bc0a5e70e3231d820089f703751a8a1664cde
Hany Fekry <hanyaly_82@hotmail.com>
2024-08-12 15:26:17 +0300

    Create Basket api priject

---
+++ src/Services/Basket/BasketApi/GlobalUsing.cs
@@ -0,0 +1,7 @@
+﻿global using BasketApi.Models;
+global using Carter;
+global using MediatR;
+global using Mapster;
+global using BuildingBlocks.CQRS;
+global using Marten;
+global using BuildingBlocks.Exceptions;
