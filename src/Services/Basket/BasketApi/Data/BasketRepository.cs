dc43460a3a34eadf9d5664f59b32f2f7b5add739
Hany Fekry <hanyaly_82@hotmail.com>
2024-08-13 12:33:56 +0300

    add Repository for Basket

---
+++ src/Services/Basket/BasketApi/Data/BasketRepository.cs
@@ -0,0 +1,31 @@
+﻿namespace BasketApi.Data
+{
+    public class BasketRepository(IDocumentSession session) : IBasketRepository
+    {
+        public async Task<ShoppingCart> GetShoppingCartAsync(string userName, CancellationToken cancellationToken = default)
+        {
+            var cart = await session.LoadAsync<ShoppingCart>(userName, cancellationToken);
+            if (cart == null)
+                throw new ShoppingCartNotFoundException(userName);
+            return cart;
+        }
+
+        public async Task<ShoppingCart> StoreShoppingCartAsync(ShoppingCart cart, CancellationToken cancellationToken = default)
+        {
+            session.Store<ShoppingCart>(cart);
+            await session.SaveChangesAsync(cancellationToken);
+            return cart;
+        }
+
+        public async Task<bool> DeleteShoppingCartAsync(string userName, CancellationToken cancellationToken = default)
+        {
+            var cart = await session.LoadAsync<ShoppingCart>(userName, cancellationToken);
+            if (cart == null)
+                throw new ShoppingCartNotFoundException(userName);
+            session.Delete(userName);
+            await session.SaveChangesAsync(cancellationToken);
+            return true;
+        }
+    }
+
+}
---
+++ src/Services/Basket/BasketApi/Data/IBasketRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace BasketApi.Data
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetShoppingCartAsync(string userName, CancellationToken cancellationToken = default);
+        Task<bool> DeleteShoppingCartAsync(string userName, CancellationToken cancellationToken = default);
+        Task<ShoppingCart> StoreShoppingCartAsync(ShoppingCart cart, CancellationToken cancellationToken = default);
+    }
+}
