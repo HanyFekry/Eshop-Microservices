f2f3c2788501e24605e2b5a7360ad4593ab5e8c7
Hany Fekry <hanyaly_82@hotmail.com>
2024-08-16 17:15:34 +0300

    Add Ordering projects with dependencies

---
+++ src/Services/Ordering/Ordering.Api/Program.cs
@@ -0,0 +1,18 @@
+using Ordering.Api;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
+builder.Services.AddApiServices();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseApiServices();
+
+app.Run();
