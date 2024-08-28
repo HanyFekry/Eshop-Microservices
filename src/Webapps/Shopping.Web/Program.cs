22bf94b9e40eddce26f507523f53a553629a0666
Hany Fekry <hanyaly_82@hotmail.com>
2024-08-28 16:59:44 +0300

    Create ICategory interface and add reference to Refit.HttpClientFactory

--- src/Webapps/Shopping.Web/Program.cs
+++ src/Webapps/Shopping.Web/Program.cs
@@ -1,7 +1,10 @@
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddRefitClient<ICatalogService>()
+    .ConfigureHttpClient(c => c.BaseAddress = new Uri(builder.Configuration.GetValue<string>("YarpSettings:ApiGateway")!));
 
 var app = builder.Build();
 
---
+++ src/Webapps/Shopping.Web/Services/ICatalogService.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Shopping.Web.Services
+{
+    public interface ICatalogService
+    {
+        [Get("catalog-service/products?pageIndex={pageIndex}&pageSize={pageSize}")]
+        Task<GetProductsResponse> GetProductsAsync(int pageIndex, int pageSize);
+        [Get("catalog-service/products/category={category}")]
+        Task<GetProductsByCategoryResponse> GetProductsByCategoryAsync(string category);
+        [Get("catalog-service/products/{id}")]
+        Task<GetProductByIdResponse> GetProductByIdAsync(Guid id);
+    }
+}
