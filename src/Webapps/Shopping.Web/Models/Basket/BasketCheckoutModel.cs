fd672534c606a1c3bb7e4de9261bf69f058d2b06
Hany Fekry <hanyaly_82@hotmail.com>
2024-08-28 16:58:43 +0300

    Create project models

---
+++ src/Webapps/Shopping.Web/Models/Basket/BasketCheckoutModel.cs
@@ -0,0 +1,27 @@
+﻿namespace Shopping.Web.Models.Basket
+{
+    public class BasketCheckoutModel
+    {
+        public string UserName { get; set; } = default!;
+        public Guid CustomerId { get; set; } = default!;
+        public decimal TotalPrice { get; set; } = default!;
+
+        // Shipping and BillingAddress
+        public string FirstName { get; set; } = default!;
+        public string LastName { get; set; } = default!;
+        public string Email { get; set; } = default!;
+        public string Street { get; set; } = default!;
+        public string Country { get; set; } = default!;
+        public string City { get; set; } = default!;
+        public string PostalCode { get; set; } = default!;
+
+        // Payment
+        public string CardName { get; set; } = default!;
+        public string CardNumber { get; set; } = default!;
+        public string Expiration { get; set; } = default!;
+        public string Cvv { get; set; } = default!;
+        public int PaymentMethod { get; set; } = default!;
+    }
+    public record CheckoutBasketRequest(BasketCheckoutModel Basket);
+    public record CheckoutBasketResponse(bool Success);
+}
